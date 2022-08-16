7487361001f3da730de3a8e25388637c3ce9deda
Nihat Halici <nihat.halici@gmail.com>
2022-08-16 12:28:55 +0200

    Update Exer-02-Choosing-between-float-decimal-and-fraction.py

--- CHPT-01-Numbers-Strings-and-Tuples/Exer-02-Choosing-between-float-decimal-and-fraction.py
+++ CHPT-01-Numbers-Strings-and-Tuples/Exer-02-Choosing-between-float-decimal-and-fraction.py
@@ -12,3 +12,46 @@ print( total_amount.quantize(penny) )
 
 import decimal
 print( total_amount.quantize(penny, decimal.ROUND_UP) )
+
+from fractions import Fraction
+sugar_cups = Fraction('2.5')
+scale_factor = Fraction(5/8)
+print(sugar_cups * scale_factor)
+print(Fraction(24, 16))
+
+###
+
+answer = (19/155)*(155/19)
+print(round(answer, 3))
+print(1-answer)
+
+###
+
+print(float(total_amount))
+print(float(sugar_cups * scale_factor))
+
+###
+
+print(Fraction(19/155))
+print(Decimal(19/155))
+
+###
+
+print(6737037547376141 / 2 ** 53 * 2 ** 226)
+
+###
+
+import math
+
+print(math.frexp(8.066E+67))
+
+###
+
+print((19/155) * (155/19) == 1.0)
+print(math.isclose((19/155) * (155/19), 1))
+
+###
+import cmath 
+
+# math.sqrt(-2)  # ValueError: math domain error
+print(cmath.sqrt(-2))
