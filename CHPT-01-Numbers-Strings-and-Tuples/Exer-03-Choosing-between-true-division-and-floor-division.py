5bac09cbc40b44fed04ebb00b0a862d4b7fe868a
Nihat Halici <nihat.halici@gmail.com>
2022-08-21 10:10:47 +0200

    Update Exer-03-Choosing-between-true-division-and-floor-division.py

--- CHPT-01-Numbers-Strings-and-Tuples/Exer-03-Choosing-between-true-division-and-floor-division.py
+++ CHPT-01-Numbers-Strings-and-Tuples/Exer-03-Choosing-between-true-division-and-floor-division.py
@@ -15,3 +15,22 @@ total_seconds = 7385
 hours, remaining_seconds = divmod(total_seconds, 3600)
 minutes, seconds = divmod(remaining_seconds, 60)
 print(hours, minutes, seconds)
+
+###
+
+total_seconds = 7385
+hours = total_seconds / 3600
+print(round(hours, 4))
+
+###
+
+from fractions import Fraction
+total_seconds = Fraction(7385)
+
+hours = total_seconds / 3600
+print(hours)
+print(round(float(hours), 4))
+
+print(7358.0 //3600.0)
+
+from __future__ import division
