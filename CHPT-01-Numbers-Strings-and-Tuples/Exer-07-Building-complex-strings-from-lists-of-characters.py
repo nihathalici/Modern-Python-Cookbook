b38816125ff8b6b6aa2fc2ff6915b20e6c1dc282
Nihat Halici <nihat.halici@gmail.com>
2022-08-24 08:14:34 +0200

    Add files via upload

---
+++ CHPT-01-Numbers-Strings-and-Tuples/Exer-07-Building-complex-strings-from-lists-of-characters.py
@@ -0,0 +1,15 @@
+# Exer-07-Building-complex-strings-from-lists-of-characters
+
+title = "Recipe 5: Rewriting an Immutable String"
+
+from string import whitespace, punctuation
+
+title_list = list(title)
+
+colon_position = title_list.index(':')
+
+del title_list[:colon_position + 1]
+
+for position in range(len(title_list)):
+    if title_list[position] in whitespace+punctuation:
+        title_list[position] = '_'
