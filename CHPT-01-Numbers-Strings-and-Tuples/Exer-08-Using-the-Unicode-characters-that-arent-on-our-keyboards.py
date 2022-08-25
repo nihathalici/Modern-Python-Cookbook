03fc789f00e696d9e969396b7a98d08c698cc537
Nihat Halici <nihat.halici@gmail.com>
2022-08-25 08:49:56 +0200

    Add files via upload

---
+++ CHPT-01-Numbers-Strings-and-Tuples/Exer-08-Using-the-Unicode-characters-that-arent-on-our-keyboards.py
@@ -0,0 +1,6 @@
+# Exer-08-Using-the-Unicode-characters-that-arent-on-our-keyboards
+
+print("You Rolled \u2680")
+print("You drew \U0001F000")
+print("Discard \N{MAHJONG TILE RED DRAGON}")
+print(r"\w+")
