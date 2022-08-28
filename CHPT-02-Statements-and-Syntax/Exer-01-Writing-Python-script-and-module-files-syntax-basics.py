11a09ce717e9ff260273ad80edb799a018a34ad1
Nihat Halici <nihat.halici@gmail.com>
2022-08-28 11:01:30 +0200

    Update Exer-01-Writing-Python-script-and-module-files-syntax-basics.py

--- CHPT-02-Statements-and-Syntax/Exer-01-Writing-Python-script-and-module-files-syntax-basics.py
+++ CHPT-02-Statements-and-Syntax/Exer-01-Writing-Python-script-and-module-files-syntax-basics.py
@@ -1,1 +1,12 @@
+#!/usr/bin/env python3
+
 # Exer-01-Writing-Python-script-and-module-files-syntax-basics
+
+'''
+Calculate an important value.
+'''
+print(355/113)
+π=355/113
+print(π)
+color = 355/113 # type: float
+# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
