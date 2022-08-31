98bb41ea075b6a6d4a69a4ab2926c6ecd73f951f
Nihat Halici <nihat.halici@gmail.com>
2022-08-31 08:52:26 +0200

    Update Exer-03-Including-descriptions-and-documentation.py

--- CHPT-02-Statements-and-Syntax/Exer-03-Including-descriptions-and-documentation.py
+++ CHPT-02-Statements-and-Syntax/Exer-03-Including-descriptions-and-documentation.py
@@ -1,1 +1,28 @@
 # Exer-03-Including-descriptions-and-documentation
+'''
+Downloads and decodes the  current Special Marine Warning (SMW)
+for the area 'AKQ'. 
+
+SYNOPSIS
+========
+::
+    python3 akq_weather.py
+
+DESCRIPTION
+===========
+
+Downloads the Special Marine Warnings
+
+FILES
+=====
+
+Writes a file, ``AKW.html``.
+
+EXAMPLES
+========
+
+Here's an example::
+
+    slott$ python3 akq_weather.py
+    <h3>There are no products active at this time.</h3>
+'''
