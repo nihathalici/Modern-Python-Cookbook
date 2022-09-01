99890779242f42290c0333d8fdd6b03703cd30f9
Nihat Halici <nihat.halici@gmail.com>
2022-09-01 14:09:53 +0200

    Update Exer-04-Writing-better-RST-markup-in-docstrings.py

--- CHPT-02-Statements-and-Syntax/Exer-04-Writing-better-RST-markup-in-docstrings.py
+++ CHPT-02-Statements-and-Syntax/Exer-04-Writing-better-RST-markup-in-docstrings.py
@@ -1,2 +1,22 @@
 # Exer-04-Writing-better-RST-markup-in-docstrings
 
+"""
+Topic
+=====
+
+..    important::
+      Do not flip the bozo bit.
+
+..    note:: Note Title
+      We need to indent the content of an admonition.
+      This will set the text off from other material.
+..    parsed-literal::
+      any text
+          *almost* any format
+      the text is preserved
+          but **inline** markup can be used.
+          *emphasis*
+          **strong**
+          `section title`_ 
+          ``code``
+"""
