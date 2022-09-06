56609d21dab05590887d3db3367e3d3882a046b7
Nihat Halici <nihat.halici@gmail.com>
2022-09-06 11:00:18 +0200

    Update Exer-07-Avoiding-a-potential-problem-with-break-statements.py

--- CHPT-02-Statements-and-Syntax/Exer-07-Avoiding-a-potential-problem-with-break-statements.py
+++ CHPT-02-Statements-and-Syntax/Exer-07-Avoiding-a-potential-problem-with-break-statements.py
@@ -1,1 +1,15 @@
 # Exer-07-Avoiding-a-potential-problem-with-break-statements
+
+sample_1 = "some_name = the_value"
+for position in range(len(sample_1)):
+    if sample_1[position] in '=:':
+        break
+print('name=', sample_1[:position],
+       'value=', sample_1[position+1:])
+
+sample_2 = 'name_only'
+for position in range(len(sample_2)):
+    if sample_2[position] in '=:':
+        break
+print('name=', sample_2[:position], 
+      'value=', sample_2[position+1:])
