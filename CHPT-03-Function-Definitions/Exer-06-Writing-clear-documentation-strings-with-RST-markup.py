a5f267a0c003c2512373b71855b66b46c51beca0
Nihat Halici <nihat.halici@gmail.com>
2022-09-29 07:48:48 +0200

    Update Exer-06-Writing-clear-documentation-strings-with-RST-markup.py

--- CHPT-03-Function-Definitions/Exer-06-Writing-clear-documentation-strings-with-RST-markup.py
+++ CHPT-03-Function-Definitions/Exer-06-Writing-clear-documentation-strings-with-RST-markup.py
@@ -10,7 +10,22 @@ def Twc(T, V):
     :param V: Wind Speed in kph
     :returns: Wind-Chill temperature in °C 
     :raises ValueError: for win speeds under over 4.8 kph or T above 10°C 
+
+    >>> round(Twc(-10, 25), 1)
+    -18.8
+
+    See https://en.wikipedia.org/wiki/Wind_chill
+    
+    .. math::
+       T_{wc}(T_a, V) = 13.12 + 0.6215 T_a - 11.37 V^{0.16} + 0.3965 T_a V^{0.16}
+
     """
     if V < 4.8 or T > 10.0:
         raise ValueError("V must be over 4.8 kph, T must be below 10°C")
     return 13.12 + 0.6215*T - 11.37*V**0.16 + 0.3965*T*V**0.16
+
+def wind_chill_table():
+    """Uses :func: `Twc` to produce a wind-chill 
+    table for temperatures from -30 °C to 10 °C and
+    wind speeds from 5kph to 50kph.
+    """
