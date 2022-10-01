8362671ef9413f97967e5449508d9fe3369af47f
Nihat Halici <nihat.halici@gmail.com>
2022-10-01 12:51:56 +0200

    Update Exer-07-Designing-recursive-functions-around-Pythons-stack-limits.py

--- CHPT-03-Function-Definitions/Exer-07-Designing-recursive-functions-around-Pythons-stack-limits.py
+++ CHPT-03-Function-Definitions/Exer-07-Designing-recursive-functions-around-Pythons-stack-limits.py
@@ -1,5 +1,6 @@
 # Exer-07-Designing-recursive-functions-around-Pythons-stack-limits
 
+
 def fact(n: int) -> int:
     if n == 0:
         return 1
@@ -13,3 +14,18 @@ def prod(int_iter):
 
 def fact(n):
     return prod(range(1, n+1))
+
+###
+
+def ugly_fact(n):
+    if n > 0:
+        return fact(n - 1) * n
+    elif n == 0:
+        return 1
+    else:
+         raise Exception('Logic Error')
+
+p = n  
+while n != 1:
+    n = n - 1
+    p *= n
