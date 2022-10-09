257239e51d971bfbedb7a7c2f4a0c0ad3e40eee5
Nihat Halici <nihat.halici@gmail.com>
2022-10-09 13:27:54 +0200

    Add files via upload

--- CHPT-04-Built-in-Data-Structures-list-set-dict/Exer-02-Building-lists-literals-appending-and-comprehensions.py
+++ CHPT-04-Built-in-Data-Structures-list-set-dict/Exer-02-Building-lists-literals-appending-and-comprehensions.py
@@ -18,3 +18,51 @@ print(sum(file_sizes))
 ###
 
 [path.stat().st_size for path in home.glob('*/index.rst') ]
+
+###
+
+#path.stat().st_size for path in home.glob('*/index.rst') 
+
+list(path.stat().st_size for path in home.glob('*/index.rst'))
+
+some_list = [None for i in range(100)]
+
+###
+
+sizes = list(path.stat().st_size 
+    for path in home.glob('*/index.rst'))
+
+sum(sizes)
+max(sizes)
+min(sizes)
+from statistics import mean
+round(mean(sizes), 3)
+sizes.index(min(sizes))
+
+###
+
+ch1 = list(path.stat().st_size 
+    for path in home.glob('ch_01*/*.rst'))
+ch2 = list(path.stat().st_size 
+    for path in home.glob('ch_02*/*.rst'))
+len(ch1)
+len(ch2)
+final = ch1 + ch2
+len(final)
+sum(final)
+
+###
+
+final_ex = []
+final_ex.extend(ch1)
+final_ex.extend(ch2)
+len(final_ex)
+sum(final_ex)
+
+###
+
+p = [3, 5, 11, 13]
+p.insert(0, 2)
+print(p)
+p.insert(3, 7)
+print(p)
