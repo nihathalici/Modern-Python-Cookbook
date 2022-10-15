68a09af0210140c8563da522adfe6cf5d2a2c1aa
Nihat Halici <nihat.halici@gmail.com>
2022-10-15 09:09:28 +0200

    Add files via upload

--- CHPT-04-Built-in-Data-Structures-list-set-dict/Exer-05-Reversing-a-copy-of-a-list.py
+++ CHPT-04-Built-in-Data-Structures-list-set-dict/Exer-05-Reversing-a-copy-of-a-list.py
@@ -17,3 +17,12 @@ for b in 60, 60, 24, 7:
     fields.append(f)
 fields.append(t_s)
 print(fields)
+
+###
+
+fields_copy1 = fields.copy()
+fields_copy1.reverse()
+print(fields_copy1)
+
+fields_copy2 = fields[::-1]
+print(fields_copy1)
