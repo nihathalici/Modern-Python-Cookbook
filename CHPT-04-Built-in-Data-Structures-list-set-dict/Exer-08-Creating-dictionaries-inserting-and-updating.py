d3367f0cd1e8b8608d236c14076735345e3e88c2
Nihat Halici <nihat.halici@gmail.com>
2022-10-20 07:11:06 +0200

    Add files via upload

--- CHPT-04-Built-in-Data-Structures-list-set-dict/Exer-08-Creating-dictionaries-inserting-and-updating.py
+++ CHPT-04-Built-in-Data-Structures-list-set-dict/Exer-08-Creating-dictionaries-inserting-and-updating.py
@@ -1,1 +1,13 @@
 # Exer-08-Creating-dictionaries-inserting-and-updating
+
+def arrival2(n=8):
+    p = 0
+    while True:
+        step = random.choice([-1, 0, +1])
+        p += step
+        yield abs(p) % n
+
+import random
+from ch04_r06 import samples, arrival2
+random.seed(1)
+list(samples(10, arrival2(8)))
