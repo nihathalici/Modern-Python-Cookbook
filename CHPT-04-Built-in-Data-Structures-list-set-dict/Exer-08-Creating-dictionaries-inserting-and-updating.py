0a9545e7166c873312768206a1b350e5f5e27f67
Nihat Halici <nihat.halici@gmail.com>
2022-10-21 07:50:40 +0200

    Add files via upload

--- CHPT-04-Built-in-Data-Structures-list-set-dict/Exer-08-Creating-dictionaries-inserting-and-updating.py
+++ CHPT-04-Built-in-Data-Structures-list-set-dict/Exer-08-Creating-dictionaries-inserting-and-updating.py
@@ -7,7 +7,55 @@ def arrival2(n=8):
         p += step
         yield abs(p) % n
 
+
 import random
 from ch04_r06 import samples, arrival2
 random.seed(1)
 list(samples(10, arrival2(8)))
+
+###
+
+histogram = {}
+
+for customer in source:
+    if customer not in histogram:
+        histogram[customer] = 0
+    histogram[customer] += 1
+
+###
+
+histogram = {}
+for customer in source:
+    histogram.setdefault(customer, 0)
+    histogram[customer] += 1
+
+###
+
+from collections import defaultdict
+
+def summarize_3(source):
+    histogram = defaultdict(int)
+    for item in source:
+        histogram[item] += 1
+    return histogram
+
+###
+
+from collections import Counter
+
+def summarize_4(source):
+    histogram  = Counter(source)
+    return histogram
+
+###
+
+import random
+from pprint import pprint
+
+random.seed(1)
+
+histogram = summarize_4(samples(1000, arrival2(8)))
+pprint(histogram)
+
+for key in sorted(histogram):
+    print(key, histogram[key])
---
+++ CHPT-04-Built-in-Data-Structures-list-set-dict/Exer-09-Removing-from-dictionaries-the-pop-method-and-the-del-statement.py
@@ -0,0 +1,2 @@
+# Exer-09-Removing-from-dictionaries-the-pop-method-and-the-del-statement
+
