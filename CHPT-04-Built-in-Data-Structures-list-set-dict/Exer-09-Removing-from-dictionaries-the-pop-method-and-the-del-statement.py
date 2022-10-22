bf40343d151ca34397ba964d535ef0ff70fcae42
Nihat Halici <nihat.halici@gmail.com>
2022-10-22 12:39:57 +0200

    Add files via upload

--- CHPT-04-Built-in-Data-Structures-list-set-dict/Exer-09-Removing-from-dictionaries-the-pop-method-and-the-del-statement.py
+++ CHPT-04-Built-in-Data-Structures-list-set-dict/Exer-09-Removing-from-dictionaries-the-pop-method-and-the-del-statement.py
@@ -1,2 +1,14 @@
 # Exer-09-Removing-from-dictionaries-the-pop-method-and-the-del-statement
 
+working_bets = {}
+#working_bets[bet_name] = bet_amount
+working_bets['pass'] = 1
+del working_bets['come odds']
+amount = working_bets.pop('come odds')
+working_bets["come"] = 1
+working_bets["come"] = None
+
+for bet_name in working_bets:
+    print(bet_name, working_bets[bet_name])
+
+
---
+++ CHPT-04-Built-in-Data-Structures-list-set-dict/Exer-10-Controlling-the-order-of-dict-keys.py
@@ -0,0 +1,1 @@
+# Exer-10-Controlling-the-order-of-dict-keys
