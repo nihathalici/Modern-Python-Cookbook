40bd7aac41407cdba99a66353da08f1f28f3b398
Nihat Halici <nihat.halici@gmail.com>
2022-10-23 12:49:58 +0200

    Add files via upload

--- CHPT-04-Built-in-Data-Structures-list-set-dict/Exer-10-Controlling-the-order-of-dict-keys.py
+++ CHPT-04-Built-in-Data-Structures-list-set-dict/Exer-10-Controlling-the-order-of-dict-keys.py
@@ -1,1 +1,9 @@
 # Exer-10-Controlling-the-order-of-dict-keys
+from pathlib import Path
+import csv
+data_path = Path('craps.csv')
+with data_path.open() as data_file:
+    reader = csv.DictReader(data_file)
+    data = list(reader)
+for row in data:
+    print(row)
