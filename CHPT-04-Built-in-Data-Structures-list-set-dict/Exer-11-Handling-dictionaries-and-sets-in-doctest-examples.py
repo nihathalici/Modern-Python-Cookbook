f4dad92c316ab9e82e4ce1efa083f68e3cf63ba7
Nihat Halici <nihat.halici@gmail.com>
2022-10-26 09:45:54 +0200

    Update Exer-11-Handling-dictionaries-and-sets-in-doctest-examples.py

--- CHPT-04-Built-in-Data-Structures-list-set-dict/Exer-11-Handling-dictionaries-and-sets-in-doctest-examples.py
+++ CHPT-04-Built-in-Data-Structures-list-set-dict/Exer-11-Handling-dictionaries-and-sets-in-doctest-examples.py
@@ -1,1 +1,14 @@
 # Exer-11-Handling-dictionaries-and-sets-in-doctest-examples
+
+words = set(
+'''Beautiful is better than ugly.
+Explicit is better than implicit.
+Simple is better than complex.
+Complex is better than complicated.
+'''.replace('.', ' ').split())
+
+#print(words)
+
+print(list(sorted(words)))
+
+#list(sorted(some_dictionary.items()))
