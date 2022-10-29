796577903f8b1dd3bfe74926ec223020d2ed71fb
Nihat Halici <nihat.halici@gmail.com>
2022-10-29 09:35:11 +0200

    Update Exer-13-Making-shallow-and-deep-copies-of-objects.py

--- CHPT-04-Built-in-Data-Structures-list-set-dict/Exer-13-Making-shallow-and-deep-copies-of-objects.py
+++ CHPT-04-Built-in-Data-Structures-list-set-dict/Exer-13-Making-shallow-and-deep-copies-of-objects.py
@@ -16,3 +16,38 @@ some_list = [[2, 3, 5], [7, 11, 13]]
 another_list = some_list.copy()
 print(some_list is another_list)
 print(some_list[0] is another_list[0])
+
+###
+
+import copy
+
+some_dict = {'a': [1, 1, 2, 3]}
+another_dict = copy.deepcopy(some_dict)
+
+some_dict['a'].append(5)
+print(some_dict)
+print(another_dict)
+
+print( id(some_dict['a']) == id(another_dict['a']) )
+
+###
+
+copy_of_list = [item for item in some_list]
+copy_of_dict = {key:value for key, value in some_dict.items()}
+
+###
+
+"""
+immutable = (numbers.Number, tuple, str, bytes)
+def deepcopy_list(some_list):
+    copy = []
+    for item in some_list:
+        if isinstance(item, immutable):
+            copy.append(item)
+        else:
+            copy.append(copy.deepcopy(item))
+"""
+
+a = [1, 2, 3]
+a.append(a)
+print(a)
