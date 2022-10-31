5e5edfff0c1ea6f21f5abbdf749c79a393e94a7a
Nihat Halici <nihat.halici@gmail.com>
2022-10-31 09:21:47 +0100

    Update Exer-14-Avoiding-mutable-default-values-for-function-parameters.py

--- CHPT-04-Built-in-Data-Structures-list-set-dict/Exer-14-Avoiding-mutable-default-values-for-function-parameters.py
+++ CHPT-04-Built-in-Data-Structures-list-set-dict/Exer-14-Avoiding-mutable-default-values-for-function-parameters.py
@@ -28,3 +28,35 @@ print(s3)
 
 print(s1 is s3)
 print(s1)
+
+###
+
+def gather_stats(n, samples=1000, summary=None):
+    if summary is None: summary = Counter()
+
+###
+
+def create_stats(n, samples=1000):
+    return update_stats(n, samples, Counter())
+def update_stats(n, samples=1000, summary):
+    summary.update(
+        sum(randint(1, 6) for d in range(n))
+          for _ in range(samples))
+
+###
+
+def gather_stats(n, samples=1000, summary_func=lambda x: Counter(x)):
+    summary = summary_func(
+        sum(randint(1, 6) for d in range(n))
+            for _ in range(samples))
+    return summary
+
+###
+
+seed(1)
+gather_stats(2, 12, summary_func=list)
+
+###
+
+seed(1)
+gather_stats(2, 12)
