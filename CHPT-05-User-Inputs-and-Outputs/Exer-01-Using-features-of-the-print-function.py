08427c28dad8645269540a89d3657e8e98f645eb
Nihat Halici <nihat.halici@gmail.com>
2022-11-02 06:12:16 +0100

    Update Exer-01-Using-features-of-the-print-function.py

--- CHPT-05-User-Inputs-and-Outputs/Exer-01-Using-features-of-the-print-function.py
+++ CHPT-05-User-Inputs-and-Outputs/Exer-01-Using-features-of-the-print-function.py
@@ -16,3 +16,33 @@ def get_fuel_use(source_path):
 source_path = Path('fuel2.csv')
 fuel_use = get_fuel_use(source_path)
 print(fuel_use)
+
+###
+
+for leg in fuel_use:
+    start = float(leg['fuel height on'])
+    finish = float(leg['fuel height off'])
+    print("On", leg['date'],
+    'from', leg['engine on'],
+    'to', leg['engine off'],
+    'change', start-finish, 'in.')
+
+print("date", "start", "end", "depth", sep=" | ")
+for leg in fuel_use:
+    start = float(leg['fuel height on'])
+    finish = float(leg['fuel height off'])
+    print(leg['date'], leg['engine on'],
+    leg['engine off'], start-finish, sep=" | ")
+
+###
+
+for leg in fuel_use:
+    start = float(leg['fuel height on'])
+    finish = float(leg['fuel height off'])
+    print('date', leg['date'], sep='=', end=', ')
+    print('on', leg['engine on'], sep='=', end=', ')
+    print('off', leg['engine off'], sep='=', end=', ')
+    print('change', start-finish, sep="=")
+
+
+    
