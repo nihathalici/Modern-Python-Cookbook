b1a489b24e2d340bf2860578280464b58a4f952e
Nihat Halici <nihat.halici@gmail.com>
2022-11-11 09:00:05 +0100

    Update Exer-05-Using-argparse-to-get-command-line-input.py

--- CHPT-05-User-Inputs-and-Outputs/Exer-05-Using-argparse-to-get-command-line-input.py
+++ CHPT-05-User-Inputs-and-Outputs/Exer-05-Using-argparse-to-get-command-line-input.py
@@ -24,3 +24,43 @@ def display(lat1, lon1, lat2, lon2, r):
 
 from ch03_r04 import display
 display(36.12, -86.67, 33.94, -118.4, 'NM')
+
+###
+
+import argparse
+
+def get_options():
+   parser = argparse.ArgumentParser()
+   parser.add_argument('-r', action='store',
+             choices=('NM', 'MI', 'KM'), default='NM')
+   parser.add_argument('p1', action='store', type=point_type)
+   parser.add_argument('p2', action='store', type=point_type)
+   options = parser.parse_args()
+   return options
+
+def point_type(string):
+   try:
+      lat_str, lon_str = string.split(',')
+      lat = float(lat_str)
+      lon = float(lon_str)
+      return lat, lon
+   except Exception as ex:
+      raise argparse.ArgumentTypeError from ex
+
+if __name__ == "__main__":
+   options = get_options()
+   lat_1, lon_1 = options.p1
+   lat_2, lon_2 = options.p2
+   r = {'NM': NM, 'KM': KM, 'MI': MI}[options.r]
+   display(lat1, lon1, lat2, lon2, r)
+
+###
+
+"""
+python3 some_program.py *.rst
+
+parser.add_argument('file', nargs='*')
+
+for filename in options.file:
+   process(filename)
+"""
