e50c28de232c3c2bb7297c0ec6499cfb714b929b
Nihat Halici <nihat.halici@gmail.com>
2022-11-16 07:10:32 +0100

    Update Exer-07-Using-the-OS-environment-settings.py

--- CHPT-05-User-Inputs-and-Outputs/Exer-07-Using-the-OS-environment-settings.py
+++ CHPT-05-User-Inputs-and-Outputs/Exer-07-Using-the-OS-environment-settings.py
@@ -8,3 +8,16 @@ def get_options(argv=sys.argv):
     if default_units not in ('KM', 'NM', 'MI'):
         sys.exit("Invalid value for UNITS, not KM, NM, or MI")
     default_home_port = os.environ.get('HOME_PORT')
+
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-r', action='store', 
+    choices=('NM', 'MI', 'KM'), default=default_units)
+parser.add_argument('p1', action='store', type=point_type)
+parser.add_argument('p2', nargs='?', action='store', 
+    type=point_type, default=default_home_port)
+options = parser.parse_args(argv[1:])
+if options.p2 is None:
+    sys.exit("Neither HOME_PORT nor p2 argument provided.")
+return options
