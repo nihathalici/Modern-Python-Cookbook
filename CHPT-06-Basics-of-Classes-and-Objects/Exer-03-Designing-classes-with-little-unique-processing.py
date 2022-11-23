6b82c3ebb27a27e8ed8c1abece814cb33ce990d5
Nihat Halici <nihat.halici@gmail.com>
2022-11-23 07:10:10 +0100

    Add files via upload

--- CHPT-06-Basics-of-Classes-and-Objects/Exer-03-Designing-classes-with-little-unique-processing.py
+++ CHPT-06-Basics-of-Classes-and-Objects/Exer-03-Designing-classes-with-little-unique-processing.py
@@ -1,1 +1,32 @@
 # Exer-03-Designing-classes-with-little-unique-processing
+
+from collections import namedtuple
+
+Card = namedtuple('Card', ('rank', 'suit'))
+eight_hearts = Card(rank=8, suit='\N{White Heart Suit}')
+print(eight_hearts)
+print(eight_hearts.rank)
+print(eight_hearts.suit)
+print(eight_hearts[0])
+
+# eight_hearts.suit = '\N{Black Spade Suit}' # AttributeError: can't set attribute
+
+###
+
+class Player:
+    pass
+
+p = Player()
+p.stake = 100
+
+###
+
+from argparse import Namespace
+from types import SimpleNamespace
+
+Player = SimpleNamespace
+player_1 = Player(stake=100, hand=[], insurance=None, bet=None)
+player_1.bet = 10
+player_1.stake -= player_1.bet
+player_1.hand.append(eight_hearts)
+print(player_1)
---
+++ CHPT-06-Basics-of-Classes-and-Objects/Exer-04-Optimizing-small-objects-with-slots.py
@@ -0,0 +1,1 @@
+# Exer-04-Optimizing-small-objects-with-slots
