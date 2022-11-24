ade353bf524b1f3c9a4395e5475070ba7f9f926d
Nihat Halici <nihat.halici@gmail.com>
2022-11-24 11:19:09 +0100

    Update Exer-04-Optimizing-small-objects-with-slots.py

--- CHPT-06-Basics-of-Classes-and-Objects/Exer-04-Optimizing-small-objects-with-slots.py
+++ CHPT-06-Basics-of-Classes-and-Objects/Exer-04-Optimizing-small-objects-with-slots.py
@@ -1,1 +1,33 @@
 # Exer-04-Optimizing-small-objects-with-slots
+
+class Hand:
+    __slots__ = ('hand', 'bet')
+    
+    def __init__(self, bet, hand=None):
+        self.hand = hand or []
+        self.bet = bet
+    
+    def deal(self, card):
+        self.hand.append(card)
+    
+        def __repr__(self):
+            return "".format(
+                class_ = self.__class__.__name__,
+                **vars(self)
+            )
+
+from ch06_r04 import Card, Hand
+
+h1 = Hand(2)
+h1.deal(Card(rank=4, suit='♣'))
+h1.deal(Card(rank=8, suit='♡'))
+h1
+
+###
+
+h1.bet *= 2
+h1
+
+# h1.some_other_attribute = True # AttributeError:
+
+
