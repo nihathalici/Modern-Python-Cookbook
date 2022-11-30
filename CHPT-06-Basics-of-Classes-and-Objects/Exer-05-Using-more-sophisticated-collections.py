97474c77d330c4ca6eb8967c4a156686da808d19
Nihat Halici <nihat.halici@gmail.com>
2022-11-30 09:23:43 +0100

    Add files via upload

--- CHPT-06-Basics-of-Classes-and-Objects/Exer-05-Using-more-sophisticated-collections.py
+++ CHPT-06-Basics-of-Classes-and-Objects/Exer-05-Using-more-sophisticated-collections.py
@@ -1,1 +1,9 @@
 # Exer-05-Using-more-sophisticated-collections
+
+import json
+user = json.load('˜/app.json')
+system = json.load('/etc/app.json')
+application = json.load('/opt/app/default.json')
+
+from collections import ChainMap
+config = ChainMap(user, system, application)
