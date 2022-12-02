f0461117ab79004912e65365aefa2d8f7de7ca5e
Nihat Halici <nihat.halici@gmail.com>
2022-12-02 08:54:59 +0100

    Add files via upload

---
+++ CHPT-06-Basics-of-Classes-and-Objects/Exer-07-Using-properties-for-lazy-attributes.py
@@ -0,0 +1,40 @@
+# Exer-07-Using-properties-for-lazy-attributes
+
+import math
+from collections import Counter
+
+Counter({15: 7, 17: 5, 20: 4, 16: 3, 45: 1})
+
+class LazyCounterStatistics:
+    def __init__(self, raw_counter:Counter):
+        self.raw_counter = raw_counter
+    
+    @property
+    def sum(self):
+        return sum(f*v for v, f in self.raw_counter.items())
+    
+    @property
+    def count(self):
+        return sum(f for v, f in self.raw_counter.items())
+    
+    @property
+    def mean(self):
+        return self.sum / self.count
+    
+    @property
+    def sum2(self):
+        return sum(f*v**2 for v, f in self.raw_counter.items())
+    
+    @property
+    def variance(self):
+        return (self.sum2 - self.sum**2/self.count) / (self.count - 1)
+    
+    @property
+    def stddev(self):
+        return math.sqrt(self.values)
+    
+    def raw_data(n=8, limit=1000, arrival_function=arrival1):
+        expected_time = float(expected(n))
+        data = samples(limit, arrival_function(n))
+        wait_times = Counter(coupon_collector(n, data))
+        return wait_times
