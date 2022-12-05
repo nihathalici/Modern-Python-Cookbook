8534584e4da907eb32884a9f5b6fe756eb855d0f
Nihat Halici <nihat.halici@gmail.com>
2022-12-05 15:21:08 +0100

    Add files via upload

---
+++ CHPT-06-Basics-of-Classes-and-Objects/Exer-08-Using-settable-properties-to-update-eager-attributes.py
@@ -0,0 +1,43 @@
+# Exer-08-Using-settable-properties-to-update-eager-attributes
+
+leg_1 = Leg()
+leg_1.rate = 6.0  # knots
+leg_1.distance = 35.6  # nautical miles
+print("Cover {leg.distance:.1f}nm at {leg.rate:.2f}kt = {leg.time:.2f}hr".format(leg=leg_1))
+
+class Leg:
+    def __init__(self):
+        self._rate = rate
+        self._time = time 
+        self._distance = distance
+
+    @property
+    def rate(self):
+        return self._rate
+    
+    @rate.setter
+    def rate(self, value):
+        self._rate = value
+        self._calculate('rate')
+    
+    @property
+    def time(self):
+        return self._time
+    @time.setter
+    def time(self, value):
+        self._time = value
+        self._calculate('time')
+    @property
+    def distance(self):
+        return self._distance
+    @distance.setter
+    def distance(self, value):
+        self._distance = value
+        self._calculate('distance')
+
+    
+
+
+
+
+
