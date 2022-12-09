3bff4479837821c496abf823b3df34e4a801a14a
Nihat Halici <nihat.halici@gmail.com>
2022-12-09 13:21:42 +0100

    Update Exer-08-Using-settable-properties-to-update-eager-attributes.py

--- CHPT-06-Basics-of-Classes-and-Objects/Exer-08-Using-settable-properties-to-update-eager-attributes.py
+++ CHPT-06-Basics-of-Classes-and-Objects/Exer-08-Using-settable-properties-to-update-eager-attributes.py
@@ -52,3 +52,45 @@ def _calculate(self, change):
     elif compute == {'rate'}:
         self._rate = self._distance / self._time
     
+###
+
+from ch06_r08 import Leg
+
+leg_2 = Leg(distance=38.2, time=7)
+round(leg_2.rate, 2)
+leg_2.time = 6.5
+round(leg_2.rate, 2)
+
+###
+
+class Leg:
+    def __init__(self, rate=None, time=None, distance=None):
+        self._changes = deque(maxlen=2)
+        self._rate = rate
+        if rate: self._calculate('rate')
+        self._time = time
+        if time: self._calculate('time')
+        self._distance = distance
+        if distance: self._calculate('distance')
+
+###
+
+def calc_distance(self):
+    self._distance = self._time * self._rate
+
+def calc_time(self):
+    self._time = self._distance / self._rate
+
+def calc_rate(self):
+    self._rate = self._distance / self._time
+
+
+def _calculate(self, change):
+    if change not in self._changes:
+        self._changes.append(change)
+    
+    compute = {'rate', 'time', 'distance'} - set(self._changes)
+    if len(compute) == 1:
+        name = compute.pop()
+        method = getattr(self, 'calc_' + name)
+        method()
