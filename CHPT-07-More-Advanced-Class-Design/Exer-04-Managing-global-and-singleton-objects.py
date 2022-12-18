b67a1cffc622dc25ab5f0011e81511558533fa8b
Nihat Halici <nihat.halici@gmail.com>
2022-12-18 17:15:44 +0100

    Update Exer-04-Managing-global-and-singleton-objects.py

--- CHPT-07-More-Advanced-Class-Design/Exer-04-Managing-global-and-singleton-objects.py
+++ CHPT-07-More-Advanced-Class-Design/Exer-04-Managing-global-and-singleton-objects.py
@@ -1,1 +1,25 @@
 # Managing global and singleton objects
+
+"""
+for row in source:
+    count('input')
+    some_processing()
+print(counts())
+"""
+
+# counter.py
+
+from collections import Counter
+
+_global_counter = Counter()
+
+def count(key, increment=1):
+    _global_counter[key] += increment
+def counts():
+    return _global_counter.most_common()
+
+d = Dice1(1)
+for _ in range(1000):
+    if sum(d.roll()) == 7: count('seven')
+    else: count('other')
+print(counts())
