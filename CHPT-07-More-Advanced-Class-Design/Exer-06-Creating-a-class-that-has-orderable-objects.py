b5c95de9c90afd36d4efbf6e6967605702583b5c
Nihat Halici <nihat.halici@gmail.com>
2022-12-30 00:20:22 +0100

    Update Exer-06-Creating-a-class-that-has-orderable-objects.py

--- CHPT-07-More-Advanced-Class-Design/Exer-06-Creating-a-class-that-has-orderable-objects.py
+++ CHPT-07-More-Advanced-Class-Design/Exer-06-Creating-a-class-that-has-orderable-objects.py
@@ -26,3 +26,41 @@ class SortedCard:
     def __ne__(self, other):
         return (self.rank, self.suit) != (other.rank, other.suit)
 
+###
+
+class PinochleAce(AceCard, SortedCard, PinochlePoints):
+    pass
+
+class PinochleFace(FaceCard, SortedCard, PinochlePoints):
+    pass
+
+class PinochleNumber(Card, SortedCard, PinochlePoints):
+    pass
+
+###
+
+def make_card(rank, suit):
+    if rank in (9, 10):
+        return PinochleNumber(rank, suit)
+    elif rank in (11, 12, 13):
+        return PinochleFace(rank, suit)
+    else:
+        return PinochleAce(rank, suit)
+
+from ch07_r06a import make_card
+
+c1 = make_card(9, '♡')
+c2 = make_card(10, '♡')
+c1 < c2
+c1 == c1
+c1 == c2
+c1 > c2
+
+###
+
+SUITS = '\u2660\u2661\u2662\u2663'
+Spades, Hearts, Diamonds, Clubs = SUITS
+def make_deck():
+    return [make_card(r, s) for _ in range(2)
+        for r in range(9, 15)
+        for s in SUITS]
