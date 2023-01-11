cdedad432ff48ce62be378fc0b5329c76c0ca369
Nihat Halici <nihat.halici@gmail.com>
2023-01-11 15:20:16 +0100

    Add files via upload

---
+++ CHPT-08-Functional-and-Reactive-Programming-Features/Exer-01-Writing-generator-functions-with-the-yield-statement.py
@@ -0,0 +1,14 @@
+# Exer-01-Writing-generator-functions-with-the-yield-statement
+
+data = [
+    ("2016-04-24 11:05:01,462", "INFO", "module1", "Sample Message One"),
+    ("2016-04-24 11:06:02,624", "DEBUG", "module2", "Debugging"),
+    (
+        "2016-04-24 11:07:03,246",
+        "WARNING",
+        "module1",
+        "Something might have gone wrong",
+    ),
+]
+
+###
