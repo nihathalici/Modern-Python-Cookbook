cbec0af9744a4e886bde1a82dbcc04f99509a407
Nihat Halici <nihat.halici@gmail.com>
2023-01-20 15:46:16 +0100

    Add files via upload

---
+++ CHPT-08-Functional-and-Reactive-Programming-Features/Exer-03-Applying-transformations-to-a-collection.py
@@ -0,0 +1,17 @@
+# Exer-03-Applying-transformations-to-a-collection
+
+"""
+for item in source:
+    new_item = some transformation of item
+    yield new_item
+
+
+def start_gen(tail_gen):
+    for row in tail_gen:
+        new_row = start_datetime(row)
+        yield new_row
+
+
+start_gen = (start_datetime(row) for row in tail_gen)
+
+"""
