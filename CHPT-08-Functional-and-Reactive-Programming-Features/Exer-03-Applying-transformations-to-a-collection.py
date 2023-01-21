a8624ab1d5a303beb1169e88b2ca1c205ff6d044
Nihat Halici <nihat.halici@gmail.com>
2023-01-21 15:09:29 +0100

    Update Exer-03-Applying-transformations-to-a-collection.py

--- CHPT-08-Functional-and-Reactive-Programming-Features/Exer-03-Applying-transformations-to-a-collection.py
+++ CHPT-08-Functional-and-Reactive-Programming-Features/Exer-03-Applying-transformations-to-a-collection.py
@@ -15,3 +15,37 @@ def start_gen(tail_gen):
 start_gen = (start_datetime(row) for row in tail_gen)
 
 """
+
+data = [
+    ("2016-04-24 11:05:01,462", "INFO", "module1", "Sample Message One"),
+    ("2016-04-24 11:06:02,624", "DEBUG", "module2", "Debugging"),
+    (
+        "2016-04-24 11:07:03,246",
+        "WARNING",
+        "module1",
+        "Something might have gone wrong",
+    ),
+]
+
+import datetime
+
+
+def parse_date_iter(source):
+    for item in source:
+        date = datetime.datetime.strptime(item[0], "%Y-%m-%d %H:%M:%S,%f")
+        new_item = (date,) + item[1:]
+        yield new_item
+
+
+###
+
+for row in parse_date_iter(data):
+    print(row[0], row[3])
+
+###
+
+"""
+for item in source:
+    new_item = transformation(item)
+    yield new_item
+"""
