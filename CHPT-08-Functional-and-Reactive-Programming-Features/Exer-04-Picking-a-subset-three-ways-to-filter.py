3a1548c92c4e1ca4c0c87a9716910b26cbd82274
Nihat Halici <nihat.halici@gmail.com>
2023-01-26 08:11:26 +0100

    Add files via upload

---
+++ CHPT-08-Functional-and-Reactive-Programming-Features/Exer-04-Picking-a-subset-three-ways-to-filter.py
@@ -0,0 +1,50 @@
+# Exer-04-Picking-a-subset-three-ways-to-filter
+"""
+def skip_header_date(rows):
+    for row in rows:
+        if row[0] == "date":
+            continue
+        yield row
+
+
+def row_merge(source_iter):
+    group = []
+    for row in source_iter:
+        if len(row[0]) != 0:
+            if group:
+                yield group
+            group = row.copy()
+        else:
+            group.extend(row)
+    if group:
+        yield group
+"""
+
+###
+
+import pprint
+
+from pathlib import Path
+import csv
+
+with Path("fuel.csv").open() as source_file:
+    reader = csv.reader(source_file)
+    log_rows = list(reader)
+print(log_rows[0])
+print(log_rows[-1])
+
+
+def row_merge(source_iter):
+    group = []
+    for row in source_iter:
+        if len(row[0]) != 0:
+            if group:
+                yield group
+            group = row.copy()
+        else:
+            group.extend(row)
+    if group:
+        yield group
+
+
+pprint(list(row_merge(log_rows)))
