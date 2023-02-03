1c3696df1285d604db93d82e5ba88565022f5d7a
Nihat Halici <nihat.halici@gmail.com>
2023-02-03 11:48:38 +0100

    Update Exer-05-Summarizing-a-collection-how-to-reduce.py

--- CHPT-08-Functional-and-Reactive-Programming-Features/Exer-05-Summarizing-a-collection-how-to-reduce.py
+++ CHPT-08-Functional-and-Reactive-Programming-Features/Exer-05-Summarizing-a-collection-how-to-reduce.py
@@ -18,3 +18,39 @@ def factorial(n):
 # print(factorial(52))
 
 print(factorial(52) // (factorial(5) * factorial(52 - 5)))
+
+###
+
+def reduce(function, iterable, base):
+    result = base
+    for item in iterable:
+        result = function(result, item)
+    return result
+
+###
+
+def mul(a, b):
+    return a * b
+
+add = lambda a, b: a + b
+mul = lambda a, b: a * b
+
+###
+
+def prod2(values):
+    return reduce(lambda a, b: a * b, values, 1)
+
+###
+
+from operator import add, mul 
+
+###
+
+def mymax(sequence):
+    try:
+        base = sequence[0]
+        max_rule = lambda a, b: a if a > b else b 
+        reduce(max_rule, sequence, base)
+    except IndexError:
+        raise ValueError
+
